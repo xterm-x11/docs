cf300bb8ab04446eda0e557fd865457af8b2eb9a
Max Leonov <mleonov@redhat.com>
2022-05-27 15:34:27 +0200

    chore: add a preliminary version of downstreaming script to pull content from invisible-island.net

---
+++ downstream-invisible-island.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Downstreaming xterm-related pages from Thomas Dickey's https://invisible-island.net/
+
+cd xterm-on-invisible-island
+
+wget https://invisible-island.net/xterm/xterm.html
+wget https://invisible-island.net/xterm/xterm.faq.html
+wget https://invisible-island.net/xterm/xterm.icon.html
+wget https://invisible-island.net/personal/git-exports.html
+wget https://invisible-island.net/xterm/manpage/xterm.html
+wget https://invisible-island.net/xterm/manpage/uxterm.html
+wget https://invisible-island.net/xterm/manpage/koi8rxterm.html
+wget https://invisible-island.net/xterm/ctlseqs/ctlseqs-contents.html
+wget https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+wget https://invisible-island.net/xterm/xterm.log.html
+wget https://invisible-island.net/vttest/vttest.html
+wget https://invisible-island.net/vttest/CHANGES.html
+wget https://invisible-island.net/vttest/CHANGES
+wget https://invisible-island.net/xterm/xtoolkit/xtoolkit.html
+wget https://invisible-island.net/luit/luit.html
+wget https://invisible-mirror.net/archives/xterm/
