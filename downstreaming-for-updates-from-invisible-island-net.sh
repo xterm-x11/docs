b47b390ba89de6f1902b337cc6ff038ddd16454c
Max Leonov <mleonov@redhat.com>
2022-08-14 22:19:55 +0200

    Add xft.html to the script

--- downstreaming-for-updates-from-invisible-island-net.sh
+++ downstreaming-for-updates-from-invisible-island-net.sh
@@ -21,6 +21,8 @@ wget https://invisible-island.net/vttest/vttest.html
 wget https://invisible-island.net/vttest/CHANGES.html
 wget https://invisible-island.net/vttest/CHANGES
 #
+wget https://invisible-island.net/xterm/xft/xft.html
+#
 wget https://invisible-island.net/luit/manpage/luit.html #check all links
 mv luit.html manpage-luit.html
 wget https://invisible-island.net/luit/manpage/luit.txt
