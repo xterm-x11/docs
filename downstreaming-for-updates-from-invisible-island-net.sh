8ecf8ae35b9f9a20a045f431a39980685624a307
Max Leonov <mleonov@redhat.com>
2022-10-28 17:53:25 +0200

    edits

--- downstreaming-for-updates-from-invisible-island-net.sh
+++ downstreaming-for-updates-from-invisible-island-net.sh
@@ -3,7 +3,7 @@
 
 cd xterm-on-invisible-island
 
-rm *
+git switch -c downstreaming
 
 #recurrent downstreaming to fetch updates from https://invisible-island.net/:
 #(This list is for the pages that are to be downloaded regularly to fetch updates.)
@@ -50,3 +50,34 @@ wget -r https://invisible-island.net/archives/vttest/ # --level=NUMBER ?
 wget -r https://invisible-island.net/archives/luit/ # --level=NUMBER ?
 #
 # see more links to manpages under https://invisible-island.net/xterm/xterm.faq.html#more_info
+
+#Downstreaming xterm-related pages from Thomas Dickey's https://invisible-island.net/
+
+#copy information to be published on xterm.org from the xterm-related pages on https://invisible-island.net/ that are not expected to be updated:
+#(This list is for the pages that are to be downloaded only once (or so).)
+wget https://invisible-island.net/xterm/xterm.html
+wget https://invisible-island.net/xterm/xterm.faq.html
+wget https://invisible-island.net/xterm/xterm.icon.html
+#
+wget https://invisible-island.net/personal/git-exports.html #Xorg libs used by XTerm and apps related to those libs
+#
+wget https://invisible-island.net/xterm/xtoolkit/xtoolkit.html #X Toolkit links
+wget https://github.com/freedesktop/xorg-libXt/commit/85eb751e4e1683af6cee3ee9dce29f74911a639d
+wget https://invisible-island.net/xterm/xtoolkit/intrinsics.html
+wget https://invisible-island.net/xterm/xtoolkit/intrinsics.pdf
+wget https://invisible-island.net/xterm/xtoolkit/manpage-index.html #include all linked pages after the first link because those are separate pages
+wget http://www.bitsavers.org/pdf/dec/dtj/dtj_v02-03_1990.pdf #download source
+#
+https://invisible-island.net/personal/git-exports.html#others # see what can be linked or forked under Other stuff, and where to incorporate it in the docs website
+#
+https://www.in-ulm.de/~mascheck/various/xterm/
+
+git add .
+
+git commit -m "downstreaming"
+
+git push -f origin downstreaming
+
+git switch main
+
+git branch -d downstreaming
