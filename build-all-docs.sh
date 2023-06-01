6631a97f5db1e0230b61b18d0092bab57aaf6fa6
Max Leonov <mleonov@redhat.com>
2023-06-01 23:38:59 +0200

    Create build-all-docs.sh

---
+++ build-all-docs.sh
@@ -0,0 +1,17 @@
+# List prerequisites
+# 1. Update local mks docs
+# 2. Update local material theme for mks docs
+
+# run backup-invisible-xterm.sh
+sh get-xterm-related-updates-from-invisible-island-net.sh
+
+# run "the command to build mks docs (see mks docs docs)"
+
+# run the script for downstreaming xterm.log.html
+sh downstream-release-notes.sh
+
+# run the script for downstreaming a.html
+
+# run the script for downstreaming b.html
+
+# run the script for downstreaming c.html
