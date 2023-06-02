e9a35e812e616285c35a46f746431ae982cdce3e
Max Leonov <mleonov@redhat.com>
2023-06-02 17:11:13 +0200

    Update downstream-release-notes.sh

--- tools/downstream-release-notes.sh
+++ tools/downstream-release-notes.sh
@@ -1,2 +1,21 @@
-# https://invisible-island.net/xterm/xterm.log.html
+# The original file is https://invisible-island.net/xterm/xterm.log.html
 
+cp ../xterm-on-invisible-island/xterm.log.html ../stage/release-notes-nav.html ../stage/release-notes-content.html
+
+# get the line number for this HTML element, pipe it as a variable with read, and delete the first lines of the file excluding the line of this HTML element
+sed -n '/<div class="nav">/=' ../stage/release-notes-nav.html | (read ; LINEBEFORE=$(( $REPLY - 1 )); sed -i "1,$LINEBEFORE d" ../stage/release-notes-nav.html)
+
+# get the line number for this HTML element, pipe it as a variable with read, and delete the last lines of the file excluding the line of this HTML element
+sed -n '/<\/div>/=' ../stage/release-notes-nav.html | (read ; LINEAFTER=$(( $REPLY + 1 )); sed -i "$LINEAFTER,$ d" ../stage/release-notes-nav.html)
+
+# get the line number for this HTML element, pipe it as a variable with read, and delete the first lines of the file including the line of this HTML element
+sed -n '/<\/div>/=' ../stage/release-notes-content.html | (read ; sed -i "1,$REPLY d" ../stage/release-notes-content.html)
+
+# get the line number for this HTML element, pipe it as a variable with read, and delete the last lines of the file including the line of this HTML element
+sed -n '/<\/body>/=' ../stage/release-notes-content.html | (read ; sed -i "$REPLY,$ d" ../stage/release-notes-content.html)
+
+# concatenate both file excerpts
+cat ../stage/release-notes-nav.html ../stage/release-notes-content.html > ../stage/downstreamed-complete-release-notes-page.html
+
+# delete both file excerpts
+rm ../stage/release-notes-nav.html ../stage/release-notes-content.html
