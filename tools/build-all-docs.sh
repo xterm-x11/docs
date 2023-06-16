4af51bb3f41930ff997c4b9a5d1340dc954ff4e5
Max Leonov <mleonov@redhat.com>
2023-06-16 23:39:07 +0200

    script updates, work in progress

--- tools/build-all-docs.sh
+++ tools/build-all-docs.sh
@@ -3,16 +3,32 @@
 # 2. Update local material theme for mks docs.
 # 3. The scipts require Bash.
 
+git switch main
+git pull
+git switch -c downstream
+
 # run the script that wgets up-to-date XTerm content from invisible-island.net
-sh get-xterm-related-updates-from-invisible-island-net.sh
+get-xterm-related-updates-from-invisible-island-net.sh
 
 # run "the command to build mks docs (see mks docs docs)"
+cd ..
+
+mkdocs build
+
+cd tools
 
 # run the script for downstreaming xterm.log.html
-sh downstream-release-notes.sh
+downstream-release-notes.sh
 
 # run the script for downstreaming a.html
 
 # run the script for downstreaming b.html
 
 # run the script for downstreaming c.html
+
+cd ..
+
+mkdocs serve
+
+firefox http://127.0.0.1:8000/
+
