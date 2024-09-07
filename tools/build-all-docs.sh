93166999b19bddc17ddc753257c9c5a98c3a17bb
Max Leonov <mleonov@redhat.com>
2024-09-07 14:20:21 +0200

    continue developing tools/build-all-docs.sh

--- tools/build-all-docs.sh
+++ tools/build-all-docs.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Prerequisites
 #   Download the docs repo: https://github.com/xterm-x11/docs
 #   Download the website repo https://github.com/xterm-x11/xterm-x11.github.io
@@ -6,15 +7,22 @@
 #   Update the local material theme for mks docs.
 #   The scipts require Bash.
 
+
 git switch main
 git pull
-git switch -c downstream
+# git switch -c downstream
 
 # run the script that wgets up-to-date XTerm content from invisible-island.net
-./get-xterm-related-updates-from-invisible-island-net.sh
+# ./get-xterm-related-updates-from-invisible-island-net.sh
+
+set -e
+
+git checkout -b website_build_$(date -I)
+
+cd ~/docs/
 
 # run "the command to build mks docs (see mks docs docs)"
-cd ..
+# cd ..
 
 mkdocs build
 
@@ -29,6 +37,21 @@ cd tools
 ./downstream-manpage-vttest.sh
 ./downstream-manpage-xterm.sh
 ./downstream-release-notes.sh
+./get-xterm-related-updates-from-invisible-island-net.sh
+
+git add .
+
+git commit -m "website build at $(date -Iminutes)"
+
+git log -1 --oneline
+
+git push -u origin website_build_$(date -I)
+
+git switch main
+
+git branch -D website_build_$(date -I)
+
+echo "https://github.com/xterm-x11/docs/pull/new/website_build_$(date -I)"
 
 <<comment
 
